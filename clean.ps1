c8610cf4bef14ba92c2688a7740d2c28f116a646
snkkid <florian.poizac@gmail.com>
2024-10-07 09:29:42 +0200

    add nuget clean

--- clean.ps1
+++ clean.ps1
@@ -1,10 +1,11 @@
 yarn cache clean
 pip cache remove *
 npm cache clean --force
+nuget locals all -clear
 
 docker builder prune -f
 net stop com.docker.service
 taskkill /IM "docker.exe" /F
 taskkill /IM "Docker Desktop.exe" /F
 wsl --shutdown
-Optimize-VHD -Path C:\Users\fpoizac\AppData\Local\Docker\wsl\data\ext4.vhdx -Mode Full
+Optimize-VHD -Path D:\Docker\wsl\data\ext4.vhdx -Mode Full
